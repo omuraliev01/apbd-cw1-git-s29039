78a859c2c03dba00e799e59aeeab62df6b5132c6
Yryskeldi Omuraliev <s29039@pjwstk.edu.pl>
2026-03-17 20:53:52 +0100

    Change empty input error to Polish

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter some numbers seperated by commas:");
+string input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("Please enter numbers (empty input not allowed)");
+    return
+}
