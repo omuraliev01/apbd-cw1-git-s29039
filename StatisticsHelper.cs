168df91aec32aa32dbf954eabc54cc97b7673ad4
Yryskeldi Omuraliev <s29039@pjwstk.edu.pl>
2026-03-17 20:30:59 +0100

    Add CalculateMax method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -9,6 +9,11 @@ namespace apbd-cw1-git-s29039
             if (values == null || values.Length == 0) return 0.0;
             return values.Average();
         }
+        public static int CalculateMax(int[] values)
+        {
+            if (values == null || values.Length == 0) return 0;
+            return values.Max();
+        }
 
     }
 }
