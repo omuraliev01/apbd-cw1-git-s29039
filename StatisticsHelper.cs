cfdbabac29e42fc59330a2b31f441bc35a182e3a
Yryskeldi Omuraliev <s29039@pjwstk.edu.pl>
2026-03-17 20:24:27 +0100

    Add CalculateAverage method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,6 +1,14 @@
+using System.Linq;
+
 namespace apbd-cw1-git-s29039
-
-public class StatisticsHelper
 {
+    public class StatisticsHelper
+    {
+        public static double CalculateAverage(int[] values)
+        {
+            if (values == null || values.Length == 0) return 0.0;
+            return values.Average();
+        }
 
+    }
 }
