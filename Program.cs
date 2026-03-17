eccdf48c7a57c9f6d747dbcb5d88a777cad1bd1b
Yryskeldi Omuraliev <s29039@pjwstk.edu.pl>
2026-03-17 20:54:47 +0100

    English error message for empty input

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter some numbers seperated by commas:");
+string input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("ERROR: No numbers provided!");
+    return
+}
